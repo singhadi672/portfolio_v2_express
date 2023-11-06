05ff465763ceb3d8d6e337bb6cb3e00a6ae2385f
singhadi672 <70839024+singhadi672@users.noreply.github.com>
2023-11-06 23:51:55 +0530

    fix

--- database/connection.db.js
+++ database/connection.db.js
@@ -7,17 +7,19 @@ const mongoose = require('mongoose')
 const dbName = "portfolio_v2"
 
 
-async function dbConnection() {
+const connectionStr = `mongodb+srv://MongoDbUser:aditya1997@neog-cluster.d8w2z.mongodb.net/${dbName}?retryWrites=true&w=majority`
+const dbConnection = async () => {
   try {
-    await mongoose.connect(
-      `mongodb+srv://MongoDbUser:aditya1997@neog-cluster.d8w2z.mongodb.net/${dbName}`,
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    );
 
-    console.log(`connection successfull with database ${dbName}`);
-  } catch (err) {
-    console.log(err);
+    const response = await mongoose.connect(connectionStr)
+
+    console.log("connection successful")
+
+  } catch (error) {
+    console.log('database error -->', error)
   }
 }
+
+module.exports = { dbConnection }
 
 module.exports = { dbConnection }
